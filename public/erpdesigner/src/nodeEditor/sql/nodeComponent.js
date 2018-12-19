7949b18ee9880e6a0f7f71f5c4bf952a0074551c
huangyongheng <42529657+huangyongheng@users.noreply.github.com>
2018-12-19 17:12:11 +0800

    huang2018-12-19
    于2018年12月19日保存

--- public/erpdesigner/src/nodeEditor/sql/nodeComponent.js
+++ public/erpdesigner/src/nodeEditor/sql/nodeComponent.js
@@ -1031,3 +1031,50 @@ class C_SqlNode_Like extends React.PureComponent {
     }
 }
 
+//////exists
+class C_SqlNode_Exists extends React.PureComponent {
+    constructor(props) {
+        super(props);
+        autoBind(this);
+
+        C_SqlNode_Base(this);
+        this.state = {
+            operator: this.props.nodedata.operator,
+        }
+    }
+
+    LikeChangedHandler(newoperator) {
+        var nodeData = this.props.nodedata;
+        nodeData.operator = newoperator;
+        this.setState({
+            operator: newoperator
+        });
+    }
+
+    cusHeaderFuc() {
+        if (this.ddcStyle == null) {
+            this.ddcStyle = {
+                width: '100px',
+                margin: 'auto',
+            }
+            this.outDivStyle = {
+                minWidth: '150px'
+            };
+        }
+        var nodeData = this.props.nodedata;
+        return (<div style={this.outDivStyle} f-canmove={1}>
+            <DropDownControl options_arr={['exists', 'not exists']} value={nodeData.operator} itemChanged={this.LikeChangedHandler} style={this.ddcStyle} />
+        </div>);
+    }
+    render() {
+        var nodeData = this.props.nodedata;
+        var headType = nodeData.headType == null ? 'tiny' : nodeData.headType;
+        return <C_SqlNode_Frame ref={this.frameRef} nodedata={nodeData} editor={this.props.editor} headType={headType} headText={nodeData.label} cusHeaderFuc={this.cusHeaderFuc}>
+            <div className='d-flex'>
+                <C_SqlNode_ScoketsPanel nodedata={nodeData} data={nodeData.inputScokets_arr} align='start' editor={this.props.editor} processFun={nodeData.isInScoketDynamic() ? nodeData.processInputSockets : null} nameMoveable={nodeData.scoketNameMoveable} />
+                <C_SqlNode_ScoketsPanel nodedata={nodeData} data={nodeData.outputScokets_arr} align='end' editor={this.props.editor} processFun={nodeData.isOutScoketDynamic() ? nodeData.processOutputSockets : null} nameMoveable={nodeData.scoketNameMoveable} />
+            </div>
+
+        </C_SqlNode_Frame>
+    }
+}
