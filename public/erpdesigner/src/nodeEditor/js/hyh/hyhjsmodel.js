07333375637b9ae7e40dcca21528b15f3d6b3265
huangyongheng <42529657+huangyongheng@users.noreply.github.com>
2019-04-08 15:47:17 +0800

    huangyongheng2019-4-8
    于2019-4-8日保存，完成了《出差申请》登记页面的设计

--- public/erpdesigner/src/nodeEditor/js/hyh/hyhjsmodel.js
+++ public/erpdesigner/src/nodeEditor/js/hyh/hyhjsmodel.js
@@ -2,50 +2,6 @@ const JSNODE_JSFOR = 'jsfor';
 
 class JSNode_JsFor extends JSNode_Base{
     constructor(initData, parentNode, createHelper, nodeJson) {
-<<<<<<< HEAD
-        super(initData, parentNode, createHelper, JSNODE_COMPARE, '比较', false, nodeJson);
-        autoBind(this);
-
-        if (nodeJson) {
-            if (this.outputScokets_arr.length > 0) {
-                this.outSocket = this.outputScokets_arr[0];
-                this.outSocket.type = ValueType.Boolean;
-            }
-        }
-        if (this.outSocket == null) {
-            this.outSocket = new NodeSocket('out', this, false, { type: ValueType.Boolean});
-            this.addSocket(this.outSocket);
-        }
-        this.insocketInitVal = {
-            type: ValueType.String,
-        };
-        if (this.inputScokets_arr.length == 0) {
-            this.addSocket(new NodeSocket('in0', this, true, { type: ValueType.String }));
-            this.addSocket(new NodeSocket('in1', this, true, { type: ValueType.String }));
-        }
-        else {
-            this.inputScokets_arr.forEach(socket => {
-                socket.set(this.insocketInitVal);
-            })
-        }
-        if (this.operator == null) {
-            this.operator = '==';
-        }
-    }
-
-    requestSaveAttrs() {
-        var rlt = super.requestSaveAttrs();
-        rlt.operator = this.operator;
-        return rlt;
-    }
-
-    restorFromAttrs(attrsJson) {
-        assginObjByProperties(this, attrsJson, ['operator']);
-    }
-
-    getNodeTitle() {
-        return '比较:' + this.operator;
-=======
         super(initData, parentNode, createHelper, JSNODE_SWITCH, 'For', false, nodeJson);
         autoBind(this);
 
@@ -66,7 +22,6 @@ class JSNode_JsFor extends JSNode_Base{
                 item.inputable = true;
             });
         }
->>>>>>> master
     }
 
 
@@ -84,10 +39,4 @@ JSNodeEditorControls_arr.push({
 JSNodeClassMap[JSNODE_JSFOR] = {
     modelClass: JSNode_JsFor,
     comClass: C_Node_SimpleNode,
-<<<<<<< HEAD
 };
-
-////
-=======
-};
->>>>>>> master
