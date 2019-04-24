99b0d3f9abd6a6648e0455ab7134eb23bbd68ace
dryperson <dryperson@sina.com>
2019-04-24 15:36:01 +0800

    3
    4

--- views/erppage/server/main.js
+++ views/erppage/server/main.js
@@ -110,9 +110,6 @@ function readyLogin(req, res) {
 }
 
 function getMenu() {
-    if (req.session.g_envVar == null || req.session.g_envVar.userid == null) {
-        return res.json(serverhelper.createErrorRet('请登录后使用', 1001));
-    }
     return co(function* () {
         var sql = 'SELECT [系统方案名称代码],[系统方案名称],[方案英文名称],[桌面端名称],[移动端名称],[是否列表可见],[完成确认状态],[系统流程名称],[系统流程简称] FROM [base1].[dbo].[V002C系统方案名称] where [是否列表可见] = 1 and [终止确认状态] = 0';
         var ret = yield dbhelper.asynQueryWithParams(sql);
