c1aad4bb32be9ebb88a22c570a9aaa3d3e5496b8
mac <mac@macdeMacBook-Pro.local>
2020-01-09 15:02:32 +0800

    Update backupWorker.js

--- backupWorker.js
+++ backupWorker.js
@@ -28,54 +28,51 @@ function doBackUpWork(source) {
     });
 }
 
-function backUpDir(src, dst){
+function backUpDir(src, dst) {
     // 读取目录中的所有文件/目录
     if (!fs.existsSync(dst)) {
-		fs.mkdir(dst,(a,e)=>{
-            console.log(a,e);
+        fs.mkdir(dst, (a, e) => {
+            console.log(a, e);
         });
     }
-    fs.readdir(src, function( err, childs_arr ){
-        if( err ){
-            throw err;
-        }
-  
-        childs_arr.forEach(function(childPath ){
-            var file_src = path.join(src, childPath);
-            var file_dst = path.join(dst, childPath);
-            fs.stat(file_src, function( err, stats ){
-                if( err ){
-                    throw err;
+    var childs_arr = fs.readdirSync(src);
+    childs_arr.forEach(function (childPath) {
+        var file_src = path.join(src, childPath);
+        var file_dst = path.join(dst, childPath);
+        var statInfo = fs.statSync(file_src);
+        // 判断是否为文件
+        // console.log(statInfo.size, file_src);
+        var readfile_size = statInfo.size;
+        if (statInfo.isFile()) {
+            // 创建读取流
+            var readable = fs.createReadStream(file_src);
+            // 创建写入流
+            var writable = fs.createWriteStream(file_dst);
+            // 通过管道来传输流
+            readable.pipe(writable);
+            // console.log(fs.statSync(file_dst).size)
+            writable.on('finish', function () {
+                var writefile_size = fs.statSync(file_dst).size;
+                var compare = true;
+                if (readfile_size !== writefile_size) {
+                    result = false
                 }
-                var readfile_size = stats.size;
-                // 判断是否为文件
-                if( stats.isFile() ){
-                    // 创建读取流
-                    var readable = fs.createReadStream( file_src );
-                    // 创建写入流
-                    var writable = fs.createWriteStream( file_dst ); 
-                    // 通过管道来传输流
-                    readable.pipe( writable );
-                    writable.on('finish', function () {
-                        var writefile_size = fs.statSync(file_dst).size;
-                        var compare = true;
-                        if (readfile_size !== writefile_size) {
-                            compare = false
-                        }
-                        create_log(dst, '"' + file_dst + '":"' + compare + '",');
-                    });
-                }
-                // 如果是目录则递归调用自身
-                else if( stats.isDirectory() ){
-                    backUpDir(file_src, file_dst);
-                }
+                create_log(dst, '"' + file_dst + '":"' + compare + '",');
             });
-        });
+            writable.on('error',function (err) {
+                create_log(dst,'"err":"'+err+'",')
+            })
+        }
+        // 如果是目录则递归调用自身
+        else if (statInfo.isDirectory()) {
+            backUpDir(file_src, file_dst);
+        }
     });
 }
 function create_log(dest, data) {
     fs.appendFileSync(path.join(dest, '/logger.txt'), data);
 }
+
 function check_log(src) {
     var data = fs.readFileSync(src + '/logger.txt');
     data = data.toString();
@@ -88,11 +85,10 @@ function check_log(src) {
             return;
         }
         var file_src = path.join(src, childPath);
-        // console.log(file_src, '这是check');
         var statInfo = fs.statSync(file_src);
 
         if (statInfo.isFile()) {
-            if (data[file_src] == null || data[file_src] == false ) {
+            if (data[file_src] == null || data[file_src] === false || data['err'] != null) {
                 console.log('缺失文件备份', file_src);
                 result = false;
                 return result
@@ -100,11 +96,9 @@ function check_log(src) {
         } else if (statInfo.isDirectory()) {
             check_log(file_src);
         }
-
     });
     return result
 }
-
 module.exports={
     doWork:doBackUpWork
 };
