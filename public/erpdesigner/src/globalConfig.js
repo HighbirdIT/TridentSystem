997b736d8c1cd79e08420a7fd3f99171469c0f0f
dryperson <dryperson@sina.com>
2022-06-01 10:30:04 +0800

    50
    50

--- public/erpdesigner/src/globalConfig.js
+++ public/erpdesigner/src/globalConfig.js
@@ -127,7 +127,7 @@ var Math_SQRT = 'SQRT'; //计算参数的平方根
 var Math_SIN ='SIN';
 var Math_COS ='COS';
 var Math_TAN ='TAN';
-var Math_SIGN='SING';
+var Math_SIGN='SIGN';
 var Math_arr = [Math_ABS, Math_CEILING, Math_FLOOR, Math_ROUND, Math_POWER, Math_SQRT];
 var JSMath_arr = [Math_ABS, Math_CEILING, Math_FLOOR, Math_ROUND, Math_POWER, Math_SQRT,Math_SIN,Math_COS,Math_TAN,Math_SIGN];
 //2018-23-31黄永恒编辑charfunType
